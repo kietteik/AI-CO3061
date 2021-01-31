392efcb926fabef4bb859a0ab192a728fe66b560
Kiệt <kietteikdoi@gmail.com>
2021-01-31 12:07:23 +0700

    init

--- Ass1/build/tt.py
+++
@@ -1,8 +0,0 @@
-import random as rd
-
-b = rd.sample(range(0, 11), 10)
-
-for value in b:
-    print(b[value], ":", value)
-
-print(b)
--- Ass1/result/test.py
+++
@@ -1,9 +0,0 @@
-def notConflict(board):
-    for i in range(len(board)-1):
-        lastIndex = len(board)-1
-        if (abs(board[i]-board[-1]) == lastIndex - i) or board[i] == board[-1]:
-            return False
-    return True
-
-
-print(notConflict([3, 1, 0, 2]))
